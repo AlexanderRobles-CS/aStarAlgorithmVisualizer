044aebbe36c55b6d4acd292e9d2011c4f9fe6a7d
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2023-06-14 14:46:32 -0700

    added dragging of mouse, but does not start in right position

--- aStarAlg.py
+++ aStarAlg.py
@@ -1,14 +1,12 @@
 import sys
+from PyQt6.QtWidgets import QApplication
 from user import User
 from aStarVisualizer import BoardWindow
-from PyQt6.QtWidgets import QApplication
-
 
 if __name__ == "__main__":
-
     user = User()
     user.startUp()
-    
+
     app = QApplication(sys.argv)
     window = BoardWindow(user)
     window.show()
--- aStarVisualizer.py
+++ aStarVisualizer.py
@@ -1,5 +1,41 @@
 from PyQt6.QtWidgets import QMainWindow, QPushButton, QWidget, QGridLayout
-from user import User
+from PyQt6.QtCore import Qt
+
+
+class CustomButton(QPushButton):
+    def __init__(self, board, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.board = board
+        self.user = None
+
+    def setUser(self, user):
+        self.user = user
+
+    def mousePressEvent(self, event):
+        super().mousePressEvent(event)
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.change_square_color()
+            self.board.mouse_pressed = True
+
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+        if self.board.mouse_pressed:
+            self.change_square_color()
+
+    def mouseReleaseEvent(self, event):
+        super().mouseReleaseEvent(event)
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.board.mouse_pressed = False
+
+    def change_square_color(self):
+        if not self.isStartOrFinishNode():
+            self.setStyleSheet("background-color: white;")
+
+    def isStartOrFinishNode(self):
+        startX, startY, endX, endY = self.user.returnNodes()
+        row, col = self.board.getButtonPosition(self)
+        return (row == startX and col == startY) or (row == endX and col == endY)
+
 
 class BoardWindow(QMainWindow):
     def __init__(self, user):
@@ -8,14 +44,15 @@ class BoardWindow(QMainWindow):
         self.setGeometry(100, 100, 400, 400)
         self.grid_size = 50
         self.button_size = 10
+        self.mouse_pressed = False
 
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
         layout = QGridLayout(central_widget)
-        
+
         spacing = 3  # Adjust the spacing value as desired
         layout.setSpacing(spacing)
-        
+
         # Calculate the margins to evenly distribute spacing
         total_spacing = (self.grid_size - 1) * spacing
         side_length = self.grid_size * self.button_size + total_spacing
@@ -24,28 +61,25 @@ class BoardWindow(QMainWindow):
 
         for row in range(self.grid_size):
             for col in range(self.grid_size):
-                button = QPushButton()
+                button = CustomButton(self, self)
                 button.setFixedSize(self.button_size, self.button_size)
                 button.setStyleSheet("background-color: grey;")
                 layout.addWidget(button, row, col)
 
-                button.clicked.connect(lambda _, button=button: self.change_square_color(button, user))
+                button.clicked.connect(button.change_square_color)
+                button.setUser(user)
+                button.installEventFilter(self)
 
         # Set the window size to be a square
         self.setFixedSize(side_length, side_length)
 
         central_widget.setStyleSheet("background-color: black;")
-        
+
         self.setStartAndFinishNodes(user)
 
-    def change_square_color(self, button, user):
-        if not self.isStartOrFinishNode(button, user):
+    def change_square_color(self, button):
+        if not button.isStartOrFinishNode():
             button.setStyleSheet("background-color: white;")
-
-    def isStartOrFinishNode(self, button, user):
-        startX, startY, endX, endY = user.returnNodes()
-        row, col = self.getButtonPosition(button)
-        return (row == startX and col == startY) or (row == endX and col == endY)
 
     def getButtonPosition(self, button):
         layout = self.centralWidget().layout()
@@ -67,3 +101,16 @@ class BoardWindow(QMainWindow):
                     square_button.setStyleSheet("background-color: red;")
                 else:
                     square_button.setStyleSheet("background-color: grey;")
+
+    def eventFilter(self, obj, event):
+        if event.type() == event.Type.MouseButtonPress and event.button() == Qt.MouseButton.LeftButton:
+            self.mouse_pressed = True
+            button = obj
+            self.change_square_color(button)
+        elif event.type() == event.Type.MouseButtonRelease and event.button() == Qt.MouseButton.LeftButton:
+            self.mouse_pressed = False
+        elif event.type() == event.Type.MouseMove and self.mouse_pressed:
+            button = self.childAt(event.pos())
+            if isinstance(button, CustomButton):
+                self.change_square_color(button)
+        return super().eventFilter(obj, event)
